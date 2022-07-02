42437abb2e17fa6cd8a7f807e916c8760a86e90b
akiosako <vaticano2662@gmail.com>
2022-07-02 21:16:56 +0900

    for文の追加
    for文を追加してpushしてみる

--- GitPractice.java
+++ GitPractice.java
@@ -1,6 +1,11 @@
 public class GitPractice {
     public static void main(String[] args){
         System.out.println("pushの練習");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("hello");
+            
+
+        }
     }
 
 }
